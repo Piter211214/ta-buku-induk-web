d2763726ac202839eaea2727cb3b890043b441f6
piter211214 <waduhm3@gmail.com>
2024-10-12 10:36:43 +0700

    second commit

--- src/elements/DataKelas.jsx
+++ src/elements/DataKelas.jsx
@@ -10,7 +10,7 @@ export default function DataKelas() {
       <div>
         <MenuButton className="inline-flex gap-x-1.5 w-[190px] font-sans px-3 py-2 text-sm text-gray-900 bg-slate-100 hover:bg-slate-200 active:bg-[#E2E8F0] rounded-lg">
           <img src={IconDataKelas} alt="data icon" className="h-4 w-4" />
-          Data Kelas
+          Data Kela
           <ChevronDownIcon
             aria-hidden="true"
             className="-mr-1 h-5 w-5 text-gray-400"
